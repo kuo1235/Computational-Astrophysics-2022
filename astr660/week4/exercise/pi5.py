94b868d3b0620f230fdd9f515e5cd9fb5fa23bd3
yhkuo <kuo531277@gmail.com>
2022-10-11 17:43:46 +0800

    HW2

--- astr660/week4/exercise/pi5.py
+++ astr660/week4/exercise/pi5.py
@@ -1,19 +1,32 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-print("please enter the value N you want:")
+def circle_function(x):
+    y = np.sqrt(1. - x**2)
 
-N = 10
+    return y
 
-seed1 = 10000
-seed2 = 5000
+def monte_carlo(seed1, seed2, a, b, N):
 
+    np.random.seed(seed1)                      #  
+    sample_x = a + (b-a) * np.random.rand(N)   #
+                                               #
+    np.random.seed(seed2)                      #
+    sample_y = np.random.rand(N)               #Construct random point (x, y)
+    
+    function_y = circle_function(sample_x)     #y for the real circle
+       
+    count = 0
+    
+    for i in range(N):
+        if (function_y[i] >= sample_y[i]):
+            count += 1
+  
+    answer = (count / N) * (b-a)**2
+    
+    return answer
 
-np.random.seed(seed1)
-print(np.random.random(N))
-
-np.random.seed(seed2)
-print(np.random.random(N))
-
+print(monte_carlo(1000, 10000, -1, 1, 10000))
 
 
 
